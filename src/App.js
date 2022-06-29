7c76219c6aefc5ec8402961c794bdaa93cf6a5c5
SiansandaTamara <tamarasiansanda18@gmail.com>
2022-06-29 22:33:45 +0200

    add fetch rockets files

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
+import React, { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import Missions from './components/Missions';
+import Profiles from './components/Profiles';
+import Rockets from './components/Rockets';
+import { FetchMissions } from './redux/missions/MissionSlice';
+import { displayRocket, reserveRocket, cancelReserve } from './redux/rockets/rockets';
 import './App.css';
-import Navbar from './Components/Navbar';
 
 function App() {
-  return (
-    <>
-      <Navbar />
-      {/* <Router>
-      <Routes>
-        <Route path='/' exact element ={<Myprofile />} />
-      </Routes>
-    </Router> */}
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(FetchMissions());
+  }, []);
 
-    </>
+  useEffect(() => {
+    dispatch(displayRocket());
+  }, []);
+  const reserveRocketClick = (rocket) => {
+    dispatch(reserveRocket(rocket));
+  };
+  const cancelReserveClick = (rocket) => {
+    dispatch(cancelReserve(rocket));
+  };
+
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Rockets reserveRocketClick={reserveRocketClick} cancelReserveClick={cancelReserveClick} />} />
+        <Route path="/missions" element={<Missions />} />
+        <Route path="/my-profile" element={<Profiles />} />
+      </Routes>
+    </div>
   );
 }
 
