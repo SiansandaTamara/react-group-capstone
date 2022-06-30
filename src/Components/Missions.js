8bcf821c692ebc2100d1169ae831831ea3eae625
Stanley Ezeoke <ezeokestanley.ifeanyi@gmail.com>
2022-06-30 08:20:00 +0100

    Add basic structure for rockets

---
+++ src/Components/Missions.js
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import './mission.css';
+
+const Missions = () => {
+  const missions = useSelector((state) => state.missions);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(FetchMissions());
+  }, []);
+  return (
+    <>
+      <table className="table">
+        <thead className="table-head">
+          <tr className="table-row">
+            <th>Missions</th>
+            <th className="th">Description</th>
+            <th>Status</th>
+            <th> </th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            missions.map((mission) => <MissionCard key={mission.mission_id} mission={mission} />)
+          }
+        </tbody>
+      </table>
+    </>
+  );
+};
+export default Missions;
