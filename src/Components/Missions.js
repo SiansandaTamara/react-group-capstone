fb3390609ec827229b495ae126d30097ebcf8554
Stanley Ezeoke <ezeokestanley.ifeanyi@gmail.com>
2022-06-30 11:05:16 +0100

    Add functionality for fetch mission

--- src/Components/Missions.js
+++ src/Components/Missions.js
@@ -1,25 +1,18 @@
-/* eslint-disable */
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
+import MissionCard from './Pages/MissionCard';
 import './mission.css';
 
 const Missions = () => {
   const missions = useSelector((state) => state.missions);
-
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(FetchMissions());
-  }, []);
   return (
     <>
       <table className="table">
         <thead className="table-head">
           <tr className="table-row">
-            <th>Missions</th>
+            <th className="th">Missions</th>
             <th className="th">Description</th>
-            <th>Status</th>
-            <th> </th>
+            <th className="th">Status</th>
+            <th className="th"> </th>
           </tr>
         </thead>
         <tbody>
