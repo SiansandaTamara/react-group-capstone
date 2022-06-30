a94e3b8264efb410d7c6ce6e346fbf4dda0f9ad4
Stanley Ezeoke <ezeokestanley.ifeanyi@gmail.com>
2022-06-30 07:01:38 +0100

    Add basic structure: correct linters error

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Rockets />} />
         <Route path="/missions" element={<Missions />} />
       </Routes>
     </div>
