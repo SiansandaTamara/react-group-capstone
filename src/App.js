46ad7ef1fbfb5f53200a78f97c3634cd00be77aa
Tamara Siansanda <tamarasiansanda18@gmail.com>
2022-07-01 18:13:03 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Missions from './Components/Missions';
+import { useDispatch } from 'react-redux';
+import Missions from './components/missions';
+import Profiles from './components/profiles';
+import Rockets from './components/Rockets';
+import { FetchMissions } from './redux/missions/MissionSlice';
+import { displayRocket, reserveRocket, cancelReserve } from './redux/rockets/rockets';
 import './App.css';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(FetchMissions());
+  }, []);
+
+  useEffect(() => {
+    dispatch(displayRocket());
+  }, []);
+  const reserveRocketClick = (rocket) => {
+    dispatch(reserveRocket(rocket));
+  };
+  const cancelReserveClick = (rocket) => {
+    dispatch(cancelReserve(rocket));
+  };
+
   return (
     <div className="App">
       <Routes>
+        <Route path="/" element={<Rockets reserveRocketClick={reserveRocketClick} cancelReserveClick={cancelReserveClick} />} />
         <Route path="/missions" element={<Missions />} />
+        <Route path="/my-profile" element={<Profiles />} />
       </Routes>
     </div>
   );
