dcb139c299e6353f2d186234c6043d5a97743221
SiansandaTamara <tamarasiansanda18@gmail.com>
2022-07-01 23:46:12 +0200

    fixed errors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+import Profiles from 'src/components/Profiles';
 import { displayRocket, reserveRocket, cancelReserve } from './redux/rockets/rockets';
 import { FetchMissions } from './redux/missions/MissionSlice';
 import Missions from './components/Missions';
-import Profiles from './components/Profiles';
 import Rockets from './components/Rockets';
 import './App.css';
 
