206322450636d0887149f41522d9fc849e5960c0
SiansandaTamara <tamarasiansanda18@gmail.com>
2022-07-01 18:09:11 +0200

    add files, fixed errors, add tests

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import Missions from './compo
+import Missions from './components/missions';
 import Profiles from './components/profiles';
 import Rockets from './components/Rockets';
 import { FetchMissions } from './redux/missions/MissionSlice';
